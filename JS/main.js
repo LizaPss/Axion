7ad9278345944cf4742b449811e2ab5f7da7b03d
LizaPss <udakhina2005@mail.ru>
2021-02-19 22:08:20 +0300

    aside 70%

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,22 @@
-$(document).width() > 480 ? true : false;
+
 
 $(document).ready(function() {
     $('.sneakers-conteiner').slick({
       arrows:true,
       dots:true,
+      adaptiveHeight: true,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+      speed: 1000,
+      easing: 'ease',
+      infinite: true,
+      //initialSlide:0;
+      autoplay: true,
+      autoplaySpeed: 1500,
+      //pauseOnFocuse: true,
+      //pauseOnHover: true,
+      //pauseOnDotsHover: true,
+      draggable: false,
+
     });
 })
